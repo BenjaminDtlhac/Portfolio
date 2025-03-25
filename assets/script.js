17466780e6778a229257e3633640a890bec33f96
Benjamin Dintilhac <dintilhac.benjamin@gmail.com>
2025-03-25 23:48:24 +0100

    Correction JS et mise en page

--- assets/script.js
+++ assets/script.js
@@ -4,258 +4,210 @@
 // // 2. je crée le addEventListener lorsque le curseur passe dessus
 // // 3. je crée un document qui présente ce que j'ai fait
 
+const circleToulouse = document.querySelector("#toulouse");
+const content = document.querySelector('.mapExplicationContainer');
 
+function createPresentation() {
 
-// // J'appelle ma div que je nomme divToulouse
-// const divToulouse = document.querySelector('#toulouse');
-// const divRodez = document.querySelector('.rodez');
-// const divSaintQuentin = document.querySelector('.saintQuentin');
-// const divLyon = document.querySelector('.lyon');
+    if (document.querySelector('.divToulouse')) return;
 
+   
 
+    const divPresentation = document.createElement('div');
+    divPresentation.classList.add('toulouse', 'divToulouse', 'fiche');
 
+   
 
-// // TOULOUSE
-// function createPresentationToulouse() {
+    divPresentation.innerHTML = `
+                        <h3> TOULOUSE (31) - 2009/2018</h3>
+                        
+                        <h3></h3><br>
 
-//     const presentationToulouse = document.createElement('div');
-//     presentationToulouse.classList.add('presentationToulouse', 'presentationExperience');
-//     presentationToulouse.innerHTML = `
-// <h3> Toulouse </h3>
-// <h3>1989 - 2018</h3>
-// <h4>Expériences Professionnelles</h4><br>
+                        <h4>Expériences Professionnelles</h4><br>
 
-// <h5>Mairie de Pins-Justaret (31)</h5> 
-// <h6> Responsable Finance et Comptabilité - Juin 2018 à juin 2021 </h6><br>
+                        <h5>Mairie de Pins-Justaret (31)</h5>
+                        <h6>Responsable Finance et Comptabilité - Juin 2018 à juin 2021 </h6><br>
 
-// <p>
-// - Préparation et suivi du budget de la Commune et du CCAS <br>
-// - Mise en œuvre de l'exécution comptable des marchés publics et procédures de contrôle<br>
-// - Gestion de la Trésorerie <br>
-// </p><br>
+                        <p>
+                            - Préparation et suivi du budget de la Commune et du CCAS <br>
+                            - Mise en œuvre de l'exécution comptable des marchés publics <br>et procédures de
+                            contrôle<br>
+                            - Gestion de la Trésorerie <br>
+                        </p><br>
 
 
-// <h4>Formations</h4>
-// <p>
-//  Licence professionnelle M.A.T <br>
-//  Université Jean-Jaurès Toulouse 2018 <br><br> 
+                        <h4>Formations</h4><br>
+                        <p>
+                            <strong>Licence professionnelle M.A.T</strong> <br>
+                            Université Jean-Jaurès Toulouse 2018 <br><br>
 
-//  Licence 1ere et 2ème année de droit <br>
-//  Université Capitole Toulouse 2015/2018 <br><br>
+                            Licence 1ere et 2ème année de droit <br>
+                            Université Capitole Toulouse 2015/2018 <br><br>
 
-//  Capacité en droit (distanciel) <br>
-//  IPST-CNAM Toulouse 2015 <br><br>
+                            Capacité en droit (distanciel) <br>
+                            IPST-CNAM Toulouse 2015 <br><br>
 
-//  ESICAD Ecole de de Commerce et de Gestion <br>
-//  BTS Management des Unités Commerciales - 1ère années 2009/2011 <br><br>
+                            ESICAD Ecole de de Commerce et de Gestion <br>
+                            BTS Management des Unités Commerciales - 1ère années 2009/2011 <br><br>
 
-//  Lycée Pierre Paul Riquet <br><br>
-//  Niveau Baccalauréat Littéraire 2006/2009
-//  </p>   `;
+                            Lycée Pierre Paul Riquet <br><br>
+                            Niveau Baccalauréat Littéraire 2006/2009
+    `;
 
-//     divToulouse.append(presentationToulouse);
-// }
+    content.append(divPresentation);
+    
+}
 
 
 
+function deletePresentation() {
+    const divToulouse = document.querySelector('.divToulouse');
+    divToulouse.remove();
+}
 
-// // RODEZ
-// function createPresentationRodez() {
+circleToulouse.addEventListener('mouseenter', createPresentation);
+circleToulouse.addEventListener('mouseleave', deletePresentation);
 
-//     const presentationRodez = document.createElement('div');
-//     presentationRodez.classList.add('presentationRodez', 'presentationExperience');
-//     presentationRodez.innerHTML = `
-//     <h3> Laguiole </h3>
-//     <h3>2022 - 2023</h3>
-//     <h4>Expériences Professionnelles</h4><br>
 
-//     <h5>Mairie de Laguiole (12)</h5> 
-//     <h6> Responsable Administratif et Juridique  </h6><br>
+/****************** RODEZ ***********************/
+const circleRodez = document.querySelector("#rodez");
 
-//     <p>
-//     Accompagnement de Projets : <br>
-//     - Aménagement d'une Liaison Douce entre le vieux et ancien Foirail <br>
-//     - Procédure Dissolution/Création du Syndicat Mixte Station de Ski<br><br>
+function createPresentationR() {
+    if (document.querySelector('.divRodez')) return;
 
-//     Accompagnement quotidien :<br>
-//     - Préparation phase budgétaire <br>
-//     - Rédaction des Actes Administratifs (Délibérations, Arrêtés) <br>
-//     - Présentation budgétaire au Conseil Municipal <br>
-//     - Suivi Financier des Marchés Publics <br>
-//     </p><br>
-//     `;
+    const divPresentationR = document.createElement('div');
+    divPresentationR.classList.add('rodez', 'divRodez', 'fiche');
 
-//     divRodez.append(presentationRodez);
-// }
+    divPresentationR.innerHTML = `
+                        <h3> Laguiole (12) - 2022/2023</h3>
+                        <br><br><br>
+                        <h4>Expériences Professionnelles</h4><br><br>
 
-// // SAINT-QUENTIN
-// function createPresentationSaintQuentin() {
+                        <h5>Mairie de Laguiole (12)</h5>
+                        <h6> Responsable Administratif et Juridique </h6><br><br>
 
-//     const presentationSaintQuentin = document.createElement('div');
-//     presentationSaintQuentin.classList.add('presentationSaintQuentin', 'presentationExperience');
+                        <p>
+                            <strong>Accompagnement de Projets : </strong><br>
+                            - Aménagement d'une Liaison Douce entre le vieux et ancien Foirail <br>
+                            - Procédure Dissolution/Création du Syndicat Mixte Station de Ski<br><br>
 
-//     presentationSaintQuentin.innerHTML = `
-//     <h3> Saint-Quentin </h3>
-//     <h3>2023 - 2024</h3>
-//     <h4>Expériences Professionnelles</h4><br>
+                            <strong>Accompagnement quotidien :</strong><br>
+                            - Préparation phase budgétaire <br>
+                            - Rédaction des Actes Administratifs (Délibérations, Arrêtés) <br>
+                            - Présentation budgétaire au Conseil Municipal <br>
+                            - Suivi Financier des Marchés Publics <br>
+                        </p><br>
+    `;
 
-//     <h5>Direction Départementale des Finances Publiques (02)</h5> 
-//     <h6> Conseiller aux Décideurs Locaux  </h6><br>
+    content.append(divPresentationR);
+}
 
-//     <p>
-//     Accompagnement des élus locaux suivant trois niveaux : <br>
-//     - Régulière suivant les échéances annuelles rythmant la vie de la collectivité : préparation et vote du budget <br>
-//     - Thèmatique en fonction de l'actualité des réformes : Mise en place de la réforme sur la nouvelle nomenclature comptable (M57) et le Compte Financier Unique, dématérialisation des documents budgétaires<br>
-//     - Personnalisée en fonction des besoins des collectivités : analyses financières, accompagnement dans les projets d'investissement
+function deletePresentationR() {
 
-//    </p>
-//     `;
+    const divRodez = document.querySelector('.divRodez');
+    divRodez.remove();
+}
 
-//     divSaintQuentin.append(presentationSaintQuentin);
-// }
+circleRodez.addEventListener("mouseover", createPresentationR);
+circleRodez.addEventListener('mouseleave', deletePresentationR);
 
-// // LYON
-// function createPresentationLyon() {
 
-//     const presentationLyon = document.createElement('div');
-//     presentationLyon.classList.add('presentationLyon', 'presentationExperience');
+/****************** SAINT QUENTIN ***********************/
+const circleSaintQuentin = document.querySelector("#saintQuentin");
 
-//     presentationLyon.innerHTML = `
-//     <h3> Lyon </h3>
-//     <h3>2024 - 2025</h3>
-//     <h4>Formation</h4><br>
+function createPresentationSQ() {
+    if (document.querySelector('.divSaintQuentin')) return;
 
-//     <h5>Direction Départementale des Finances Publiques (02)</h5> 
-//     <h6> Développeur Web et Web Mobile </h6><br>
+    const divPresentationSQ = document.createElement('div');
+    divPresentationSQ.classList.add('saintQuentin', 'divSaintQuentin', 'fiche');
 
-//     <p>
-//     Dans le cadre d'une reconversion professionnelle, j'ai débuté une formation auprès de la Chambre de Commerce et d'Industrie dans sa branche formation à Lyon.<br>
-//     Celle-ci a commencé en novembre 2024 et doit se terminer en octobre 2025 et me permettra d'acquérir les compétences nécessaires pour devenir Développeur Web et Web Mobile. <br>
-//     Dans le cadre de cette formation, je dois réaliser un stage de deux mois en juin et juillet 2025. C'est une opportunité que de pouvoir évoluer au sein d'une structure acceuillante et me permettre d'acquièrir des compétences supplémentaires.<br><br>
-//     Par la suite, j'envisage de réaliser une année supplémentaire en alternance pour obtenir un titre professionnelle de niveau Bac+3, et me spécialiser en back-end. Cette alternance débuterait en janvier 2026. 
+    divPresentationSQ.innerHTML = `
+                        <h3> SAINT-QUENTIN (02) - 2023/2024</h3>
+                        <h3></h3><br><br><br>
+                        <h4>Expériences Professionnelles</h4><br><br>
 
+                        <h5>Direction Départementale des Finances Publiques (02)</h5>
+                        <h6> Conseiller aux Décideurs Locaux </h6><br><br>
 
+                        <p>
+                            <strong>Accompagnement des élus locaux suivant trois niveaux : </strong><br><br>
+                            - Régulière suivant les échéances annuelles rythmant la vie de la collectivité : préparation
+                            et vote du budget <br><br>
+                            - Thèmatique en fonction de l'actualité des réformes : Mise en place de la réforme sur la
+                            nouvelle nomenclature comptable (M57) et le Compte Financier Unique, dématérialisation des
+                            documents budgétaires<br><br>
+                            - Personnalisée en fonction des besoins des collectivités : analyses financières,
+                            accompagnement dans les projets d'investissement
+    `;
 
+    content.append(divPresentationSQ);
+}
 
-//    </p>
-//     `;
+function deletePresentationSQ() {
 
-//     divLyon.append(presentationLyon);
-// }
+    const divSaintQuentin = document.querySelector('.divSaintQuentin');
+    divSaintQuentin.remove();
+}
 
+circleSaintQuentin.addEventListener("mouseover", createPresentationSQ);
+circleSaintQuentin.addEventListener('mouseleave', deletePresentationSQ);
 
 
 
 
 
 
+/****************** LYON ***********************/
+const circleLyon = document.querySelector("#lyon");
 
-// function closePresentationToulouse() {
-//     presentationToulouse = document.querySelector('.presentationToulouse');
-//     presentationToulouse.remove();
-// }
+function createPresentationL() {
 
+    if (document.querySelector('.divLyon')) return;
 
-// function closePresentationRodez() {
-//     presentationRodez = document.querySelector('.presentationRodez');
-//     presentationRodez.remove();
-// }
+    const divPresentationL = document.createElement('div');
+    divPresentationL.classList.add('lyon', 'divLyon', 'fiche');
 
-// function closePresentationSaintQuentin() {
-//     presentationSaintQuentin = document.querySelector('.presentationSaintQuentin');
-//     presentationSaintQuentin.remove();
-// }
+    divPresentationL.innerHTML = `
+                        <h3> LYON (69) - 2024/2025</h3>
+                       <br>
+                        <h4>En cours de formation</h4><br>
 
-// function closePresentationLyon() {
-//     presentationLyon = document.querySelector('.presentationLyon');
-//     presentationLyon.remove();
-// }
+                        <h6>Développeur Web et Web Mobile </h6><br>
 
+                        <p>
+                            Dans le cadre d'une reconversion professionnelle, j'ai entrepris une formation de développeur web web mobilie auprès de la
+                            Chambre de Commerce et d'Industrie dans sa branche formation à Lyon.<br><br>
+                            Cette formation a commencé en novembre 2024 et se termine en octobre 2025 avec un diplome RNCP de Niveau 5, équivalent Bac +2.    <br>
+                            Dans le cadre de cette formation, je dois réaliser un stage de deux mois en juin et juillet
+                            2025. C'est une opportunité de pouvoir évoluer au sein d'une structure me permettant de mettre 
+                            en pratique les connaissances acquises tout au long de ce cursus, et notamment découvrir l'environnement 
+                            d'un développeur au sein d'une entreprise. Le travail en équipe et son organisation scrum, 
+                            les bonnes pratiques en terme de code ou encore l'investissement dans les missions de dev sont très stimulatntes.
+                            <br><br><br>
+                            Par la suite, j'envisage de réaliser une année supplémentaire en alternance pour obtenir un
+                            titre professionnelle de niveau Bac+3. Cette alternance
+                            débutera en janvier 2026.
+                        </p>
+    `;
 
+    content.append(divPresentationL);
+}
 
+function deletePresentationL() {
 
-// // Activation/désactivation des boutons
-// divToulouse.addEventListener('mouseenter', createPresentationToulouse);
-// divToulouse.addEventListener('mouseleave', closePresentationToulouse);
+    const divLyon = document.querySelector('.divLyon');
+    divLyon.remove();
+}
 
-// divRodez.addEventListener('mouseenter', createPresentationRodez);
-// divRodez.addEventListener('mouseleave', closePresentationRodez);
+circleLyon.addEventListener("mouseover", createPresentationL);
+circleLyon.addEventListener('mouseleave', deletePresentationL);
 
-// divSaintQuentin.addEventListener('mouseenter', createPresentationSaintQuentin);
-// divSaintQuentin.addEventListener('mouseleave', closePresentationSaintQuentin);
 
-// divLyon.addEventListener('mouseenter', createPresentationLyon);
-// divLyon.addEventListener('mouseleave', closePresentationLyon);
 
-const circleToulouse = document.querySelector('#toulouse');
-const textToulouse = document.querySelector('#presentationToulouse');
 
-const circleRodez = document.querySelector('#rodez');
-const textRodez = document.querySelector('#presentationRodez');
-
-const circleSaintQuentin = document.querySelector('#saintQuentin');
-const textSaintQuentin = document.querySelector('#presentationSaintQuentin');
-
-const circleLyon = document.querySelector('#lyon');
-const textLyon = document.querySelector('#presentationLyon');
-
-console.log(textRodez);
-
-
-
-circleToulouse.addEventListener('mouseenter', () => {
-    console.log(circleToulouse)
-    textToulouse.style.display = 'block';
-    console.log(textToulouse)
-});
-
-
-circleToulouse.addEventListener('mouseleave', () => {
-    textToulouse.style.display = 'none';
-});
-
-
-
-circleRodez.addEventListener('mouseenter', () => {
-    textRodez.style.display = 'block';
-
-});
-
-circleRodez.addEventListener('mouseleave', () => {
-    textRodez.style.display = 'none';
-});
-
-
-
-circleSaintQuentin.addEventListener('mouseenter', () => {
-    textSaintQuentin.style.display = 'block';
-});
-
-circleSaintQuentin.addEventListener('mouseleave', () => {
-    textSaintQuentin.style.display = 'none';
-});
-
-
-
-circleLyon.addEventListener('mouseenter', () => {
-    textLyon.style.display = 'block';
-});
-
-circleLyon.addEventListener('mouseleave', () => {
-    textLyon.style.display = 'none';
-});
-
-
-// circleToulouse.addEventListener('mouseenter', () => {
-//     textToulouse.setAttribute('visibility', 'visible'); // Affiche le texte au clic
-// });
-
-// circleToulouse.addEventListener('mouseleave', () => {
-//     textToulouse.setAttribute('visibility', 'hidden'); // Cache le texte quand la souris sort
-// });
-
-
-
+/*******************************************************************************************/
+/*******************************************************************************************/
+/*******************************************************************************************/
 
 
 
@@ -264,16 +216,16 @@ circleLyon.addEventListener('mouseleave', () => {
 document.addEventListener("DOMContentLoaded", () => {
     const btnOn = document.querySelector(".on");
     const btnOff = document.querySelector(".off");
-    const titles = document.querySelectorAll(".icon, .wordpress, .js, .mysql");
+    const titles = document.querySelectorAll(".icon, .wordpress, .js, .mysql, .html, .css, .php, .symfony");
     const textDevs = document.querySelectorAll(".futurJob");
-    const arrowMaps = document.querySelectorAll(".frenchMap image");
-  
+    const arrowMaps = document.querySelectorAll("path");
+
 
 
     function lightToOn() {
         titles.forEach(title => {
             title.style.borderColor = "#8A2BE2";
-            title.style.boxShadow = "0 0 2px #8A2BE2, inset 0 0 2px #8A2BE2, 0 0 4px #8A2BE2, inset 0 0 4px #8A2BE2, 0 0 8px #8A2BE2, inset 0 0 8px #8A2BE2, 0 0 16px #8A2BE2, inset 0 0 16px #8A2BE2";            
+            title.style.boxShadow = "0 0 2px #8A2BE2, inset 0 0 2px #8A2BE2, 0 0 4px #8A2BE2, inset 0 0 4px #8A2BE2, 0 0 8px #8A2BE2, inset 0 0 8px #8A2BE2, 0 0 16px #8A2BE2, inset 0 0 16px #8A2BE2";
         });
 
         textDevs.forEach(textDev => {
@@ -282,7 +234,8 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
         arrowMaps.forEach(arrowMap => {
-            arrowMap.style.filter = "drop-shadow(0 0 5px #8A2BE2) drop-shadow(0 0 10px #8A2BE2)"; // Ajoute un effet lumineux aux images
+            arrowMap.style.filter = "drop-shadow(0 0 5px #8A2BE2) drop-shadow(0 0 10px #8A2BE2) drop-shadow(0 0 15px #8A2BE2)"; 
+
         });
     }
 
@@ -301,17 +254,8 @@ document.addEventListener("DOMContentLoaded", () => {
         arrowMaps.forEach(arrowMap => {
             arrowMap.style.filter = "none";
         });
-
-
     }
 
     if (btnOn) btnOn.addEventListener("click", lightToOn);
     if (btnOff) btnOff.addEventListener("click", lightToOff);
 });
-
-
-
-
-
-
-
